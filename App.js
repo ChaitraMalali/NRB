d27f6e037b0afead734cea837a8e114929c01f4a
ChaitraMalali <118448389+ChaitraMalali@users.noreply.github.com>
2022-11-18 14:17:49 +0530

    Add files via upload
    NRB- HelloWorld-FirstReactApp-Session1

---
+++ App.js
@@ -0,0 +1,13 @@
+
+       /* Hello World Using JS*/
+        const app =   document.getElementById("rootJS");
+        const heading = document.createElement("h1");
+        heading.innerHTML = "Hello JS World";
+        app.appendChild(heading);
+   
+
+
+        /*Hello World Using React !!*/
+        const reactapp = React.createElement("h1", {id : "title"}, "Hello World Using React !!");
+        const root = ReactDOM.createRoot(document.getElementById("rootReact"));
+        root.render(reactapp);
